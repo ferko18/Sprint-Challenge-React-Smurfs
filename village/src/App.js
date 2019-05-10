2c86335ec5f4d8533d9b2bcb81bd8b846d4b4be7
Firealem Erko <firealemg@yahoo.com>
2019-05-10 11:45:38 -0400

    added Router and required Routes

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -43,8 +44,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf={this.addSmurf}/>
-        <Smurfs smurfs={this.state.smurfs} />
+      
+        <Route
+          exact
+          path="/SmurfForm"
+          render={() => <SmurfForm addSmurf={this.addSmurf} />}
+        />
+        <Route
+          exact
+          path="/Smurfs"
+          render={() => <Smurfs smurfs={this.state.smurfs} />}
+        />
       </div>
     );
   }
