868f7ee9a59658cb9f488cb67ab82061eba55d59
Firealem Erko <firealemg@yahoo.com>
2019-05-10 11:05:45 -0400

    initial commit

--- village/src/App.js
+++ village/src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      test
         <SmurfForm />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
